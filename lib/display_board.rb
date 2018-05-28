5a5f203fbc470c2d8c75d8bb3897bdd264c85dfd
vanessarlogan Wentzloff <vanessarlogan@gmail.com>
2018-05-28 17:26:12 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,15 +2,15 @@
 # out the current state.
 def display_board(board)
 
-puts " #{board[0]} | #{board[1]} | #{board[2]}"
+puts "#{board[0]}|#{board[1]}|#{board[2]}"
 puts "-----------"
-puts " #{board[3]} | #{board[4]} | #{board[5]}"
+puts "#{board[3]}|#{board[4]}|#{board[5]}"
 puts "-----------"
-puts " #{board[6]} | #{board[7]} | #{board[8]}"
+puts "#{board[6]}|#{board[7]}|#{board[8]}"
 
 end
 
-display_board([" "," "," "," "," "," "," "," "," "])
+display_board(["   ","   ","   ","   ","   ","   ","   ","   ","   "])
 display_board([" "," "," "," ","X"," "," "," "," "])
 display_board(["O"," "," "," "," "," "," "," "," "])
 display_board(["O"," "," "," ","X"," "," "," "," "])
